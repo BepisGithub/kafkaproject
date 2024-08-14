4a57fc92a71bc9676f9845d2a985b272e8c68489
Bepis <luqdek@gmail.com>
2024-08-14 13:44:02 +0100

    its logging kafka events

---
+++ src/main/java/com/store/order/OrderAPI.java
@@ -0,0 +1,25 @@
+package com.store.order;
+
+import com.store.order.models.OrderEvent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+@RestController
+@RequestMapping("/order")
+public class OrderAPI {
+
+    @Autowired
+    private KafkaTemplate<Integer, OrderEvent> template;
+
+    @GetMapping("/place")
+    public String placeOrder(){
+        int orderNumber = ThreadLocalRandom.current().nextInt(1, 100 + 1);
+        this.template.send("orders", orderNumber, new OrderEvent("order", "success", orderNumber));
+        return "order " + orderNumber + " placed";
+    }
+}
